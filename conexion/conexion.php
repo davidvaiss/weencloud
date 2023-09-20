163bf1f86cdc4b3e30712cdf2e721ef6f998281c
davidvaiss <cdrueda28@misena.edu.co>
2023-09-20 16:27:58 -0500

    puta

---
+++ conexion/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$servername = "localhost";
+$database = "weencloud";
+$username = "root";
+$password = "";
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $database);
+?>
---
+++ index.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ween Cloud</title>
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Bacasime+Antique&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+    <header>
+        <div class="menu container">
+            <img src="IMG/clouds (1).png" alt="">
+            <a href="#" class="logo">Ween Cloud </a>
+            <input type="checkbox" id="menu">
+            <label for="menu">
+                <img src="IMG/menu-btn.png" class="menu-icono" alt="">
+            </label>
+            <nav class="navbar">
+                <ul>
+                   
+                    <li> <a href="registro/registro2.html">Registrarse</a></li>
+                    <li> <a href="Login/index.html">Iniciar sesion</a></li>
+                    <li> <a href="#servicios">Servicios</a></li>
+                    <li> <a href="#contacto">Contactanos</a></li>
+
+                </ul>
+            </nav>
+        </div>
+        <div class="header-content container">
+
+            <div class="content">
+
+                <h1>Somos Ween Cloud </h1>
+                <p>
+                    Somos un grupo de personas que desarrollaron este banco para facilitar la publicación y la búsqueda de los personales necesitados y empleos en la localidad cuarta de San Cristóbal
+                </p>
+                <!-- <a href="#" class="btn-1">Mas información</a> -->
+                <!-- <a href="#" class="btn-1">Mas informacion</a> -->
+            </div>
+            <img src="IMG/astronauta.png" alt="">
+        </div>
+    </header>
+
+    <main class="services container">
+        <div class="service">
+            <img src="IMG/icon1.svg" alt="">
+            <h3>Publica tu trabajo</h3>
+            <p>
+                Publica tu oferta en Ween Cloud, serán tus 5 minutos de hoy mejor invertidos
+            </p>
+            <a href="PBTRAB/index1.html" class="btn-3">Ingresar</a>
+        </div>
+        <div class="service">
+            <img src="IMG/icon2.svg" alt="">
+            <h3>Busca tu trabajo</h3>
+            <p>
+                Si buscas trabajo ¡Ween Cloud es tu mejor aliado!
+            </p>
+            <a href="BusTrabajo/index.html" class="btn-3">Ingresar</a>
+        <!-- </div>
+        <div class="service">
+            <img src="IMG/icon3.svg" alt="">
+            <h3>Servicio 3</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit
+            </p>
+        </div>
+        <div class="service">
+            <img src="IMG/icon4.svg" alt="">
+            <h3>Servicio 4</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit
+            </p>
+        </div> -->
+    </main>
+
+    <section class="porfolios container" id="servicios">
+
+        <h2>Nuestros Servicios</h2>
+
+        <div class="porfolio">
+
+            <img src="IMG/img2.png" alt="">
+            <div class="porfolio-1">
+
+                <h3>Publica tu trabajo</h3>
+                <p>
+                    En nuestro apartado si eres una persona que necesita hacer público tu emprendimiento o negocio, en este podrás proporcionar la oferta de trabajo si necesitas vacantes para esta, y hacer de esta publicación un éxito y encontrar la mejor vacante para esta.
+                </p>
+                <!-- <a href="#" class="btn-3">Informacion</a> -->
+
+            </div>
+        </div>
+
+        <div class="porfolio">
+
+            <div class="porfolio-2">
+
+                <h3>Busca tu trabajo</h3>
+                <p>
+                    Encontraras las oportunidades de empleo, las cuales tienen una mayor facilidad de búsqueda, estamos proyectados a que el usuario se sienta bien a la hora de buscare empleo que mejor opcion que Ween Cloud...
+                ¡Nunca fue tan fácil encontrar empleo!.
+ 
+                </p>
+                <!-- <a href="#" class="btn-3">Informacion</a> -->
+
+            </div>
+            <img src="IMG/img4.png" alt="">
+        </div>
+
+        <!-- <div class="porfolio">
+            <img src="IMG/img3.png" alt="">
+            <div class="porfolio-1">
+                <h3>Porfolio 3</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sed voluptatem quia ipsum culpa
+                    voluptatum tempora dolore at aperiam corporis libero nam omnis facilis rem dicta eos fugit, eveniet
+                    numquam.
+                </p>
+                <a href="#" class="btn-3">Informacion</a>
+            </div>
+        </div> -->
+
+        <!-- <div class="porfolio">
+            <div class="porfolio-2">
+                <h3>Porfolio 4</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sed voluptatem quia ipsum culpa
+                    voluptatum tempora dolore at aperiam corporis libero nam omnis facilis rem dicta eos fugit, eveniet
+                    numquam.
+                </p>
+                <a href="#" class="btn-2">Informacion</a>
+
+            </div>
+            <img src="IMG/img4.png" alt="">
+        </div> -->
+    </section>
+    <section>
+    <footer>
+        <div class="container">
+            <div class="info" id="contacto"> 
+                <div class="info-content">
+                    <div class="info-price">
+                        <h2>Contactanos</h2>
+                        <!-- <p class="price">$3500</p> -->
+                        <p>
+                            ¿En qué podemos ayudarte?
+                            Resuelve tus dudas a través de nuestras redes sociales, cualquier informacion la cual necesites nos las puedes hacer saber atraves de nuestro correo electronico, o a travez de nuestras redes sociales como instagram y Facebook. 
+                        </p>
+                    </div>
+                    <!-- <div class="info-txt">
+                        <h2>Princing list</h2>
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis deleniti obcaecati
+                            inventore nobis quod dolore quia vel ipsa rerum quae. Corporis nisi pariatur mollitia
+                            debitis ipsa delectus natus consequuntur doloremque!
+                        </p>
+                        <a href="#" class="btn-1">Informacion</a>
+                    </div> -->
+                </div>
+                <hr>
+                <div class="enlaces">
+                    <!-- <h3>Logo</h3> -->
+                    <img src="IMG/astronaut.png" alt="">
+                    <div class="enlace">
+                        <li> <a href="#">Instagram</a></li>
+                        <li> <a href="#">Facebook</a></li>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</section>
+</body>
+
+</html>
