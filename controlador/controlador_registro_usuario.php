59b3f18c2445cfe402652220f2edfc19a913b987
davidvaiss <cdrueda28@misena.edu.co>
2023-10-06 14:24:47 -0500

    sisas

--- controlador/controlador_registro_usuario.php
+++ controlador/controlador_registro_usuario.php
@@ -1,7 +1,7 @@
 <?php
 if (!empty($_POST["registro"])) {
     if (empty($_POST["nombrec"]) or empty($_POST["email"]) or empty($_POST["numero"]) empty($_POST["tdocumento"]) or empty($_POST["contraseña"]) or empty($_POST["nu_documento"]) empty($_POST["conficontraseña"]) empty($_POST["barrio"]) or empty($_POST["rol"]) or empty($_POST["genero"])) {
-        echo 
+        echo '<p>Uno de los campos esta vacio</p>'
     } else {
         $nombrec=$_POST("nombrec");
         $email=$_POST("email");
