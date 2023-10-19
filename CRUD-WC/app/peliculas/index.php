be6544f854a2c7e6fbf642a4eba271f0c8a4f2a5
davidvaiss <cdrueda28@misena.edu.co>
2023-10-19 13:23:41 -0500

    sshs

---
+++ CRUD-WC/app/peliculas/index.php
@@ -0,0 +1,82 @@
+<?php 
+
+require '../config/database.php';
+
+$sqlPeliculas = "SELECT p.id, p.nombre, p.descripcion, g.nombre AS genero FROM pelicula AS p
+INNER JOIN genero AS g ON p.id_genero=g.id";
+$peliculas = $conn->query($sqlPeliculas);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Modal</title>
+
+    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../assets/css/all.min.css" rel="stylesheet">
+</head>
+
+<body class="d-flex flex-column h-100">
+
+    <div class="container py-3">
+
+        <h2 class="text-center">Empleos</h2>
+      <div class="row justify-content-end">
+        <div class="col-auto">
+         <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalcrear"> <i class="fa-solid fa-circle-plus"></i>Nuevo Registro</button>
+
+        </div>
+     </div>    
+        <table class="table table-sm table-striped table-hover mt-4">
+            <thead class="table-dark">
+                <tr>
+                    <th>#</th>
+                    <th>Nombre</th>
+                    <th>Descripcion</th>
+                    <th>Vacante</th>
+                    <th>Lugar de Trabajo</th>
+                    <th>Experiencia</th>
+                    <th>Salario</th>
+                    <th>Jornada</th>
+                   
+            </thead>
+
+            <tbody>
+
+            <?php while ($row = $peliculas->fetch_assoc()) { ?>
+                    <tr>
+                        <td><?= $row['id']; ?></td>
+                        <td><?= $row['nombre']; ?></td>
+                        <td><?= $row['descripcion']; ?></td>
+                        <td><?= $row['genero']; ?></td>
+                        <td><?= $row['lu_trabajo']; ?></td>
+                        <td><?= $row['exp']; ?></td>
+                        <td><?= $row['sal']; ?></td>
+                        <td><?= $row['jor']; ?></td>
+                        
+                 </tr>
+                 <?php } ?>
+            </tbody>
+
+        </table>
+
+
+        <?php include 'nuevoModal.php'; ?>
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+
+    </div>
+
+    <script>src="../../assets/js/bootstrap.bundle.min.js"</script>
+
+</body>
+
+</html>
